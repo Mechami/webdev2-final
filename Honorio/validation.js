af3931aa42b0b5b2581e3f5adc617e12b9db9031
Honorio Benitez <hbenitez002@student.butte.edu>
2017-11-24 10:36:34 -0800

    this is all i'll do for the thurkey break.
    I need to ask luck about what to do  in the sing in form. for validation.

--- Honorio/validation.js
+++ Honorio/validation.js
@@ -1,34 +1,65 @@
-$().ready(function() {
-
-    $("#regiForm").validate({
-        rules: {
-            registerName: {
-                required: true,
-                minlength: 2
-            },
-            registerLastName: {
-                required: true,
-                minlength: 2
-            },
-            password: {
-                required: true,
-                minlength: 5
-            },
-            verifyPassword: {
-                required: true,
-                minlength: 5,
-                equalTo: "#registerPassword"
-            },
-            // email: {
-            //     required: true,
-            //     email: true
-            // },
-
-
+//registration form
+$("#regiForm").validate({
+    rules: {
+        registerFirstName: {
+            required: true,
+            minlength: 2
+        },
+        registerLastName: {
+            required: true,
+            minlength: 2
+        },
+        registerPassword: {
+            required: true,
+            minlength: 5
+        },
+        verifyPassword: {
+            required: true,
+            minlength: 5,
+            equalTo: "#registerPassword"
+        },
+        email: {
+            required: true,
+            email: true,
+            minlength: 7
+        },
+        registerUsername: {
+            required: true,
+            minlength: 3
+        },
+        registerAddress: {
+            required: true,
+            minlength: 2
+        },
+        registerAddress2: {
+            required: false,
+        },
+        registerCity: {
+            required: true,
+            minlength: 3
+        },
+        registerState: {
+            required: true
+        },
+        registerZip: {
+            required: true,
+            minlength: 5,
+            maxlength: 5,
+            digits: true
+        },
+        registerCheck: {
+            required: false
         }
-        
+    }
+});
 
+// sing in form
+$("singInForm").validate({
+    rules: {
+        // i still do not unerstand the validation for the singInForm, 
+        // I will be saving time, by waithing and ask to luck.
 
+        //I think that this is all i will do for the thurkey is giving break.
 
-    });
+    }
 });
